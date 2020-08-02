897d60ce2aeb02160a33581c79236c2dfdbc95fe
hugogomess <hugogomes02@gmail.com>
2020-08-02 13:29:03 -0300

    add -h flag

--- script.sh
+++ script.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # Usage: ./script.sh <cyberdrop album's link>
 
-album_name=$(curl $1 | grep 'title has-text-centered' | cut -d '"' -f6 | head -n1);
-mkdir "$album_name";
-cd "$album_name";
+if [ "$1" == "" ] || [ "$1" == "-h" ]; then
+    echo 'Usage: ./script.sh https://cyberdrop.me/a/any-album'
+else
+    album_name=$(curl $1 | grep 'title has-text-centered' | cut -d '"' -f6 | head -n1);
+    mkdir "$album_name";
+    cd "$album_name";
 
-curl $1 | grep 'id="file"' | cut -d '"' -f6 > links;
+    curl $1 | grep 'id="file"' | cut -d '"' -f6 > links;
 
-cat links | while read -r link; do
-    wget "$link";
-done;
-
+    cat links | while read -r link; do
+        wget "$link";
+    done;
+fi
 exit
