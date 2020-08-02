66c021adac2bc09133b9a300a60176fd66a76a64
hugogomess <hugogomes02@gmail.com>
2020-08-02 14:18:26 -0300

    add color on install script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,8 +2,11 @@
 # dependencies install script for cyberdrop-downloader
 
 VER='1.0';
+COLOR1='\033[91m';
+RESET='\e[0m';
 
-echo '+ -- --=[ Cyberdrop Downloader v'$VER 'by @hugogomess';
+echo -e "$COLOR1 + -- --=[ Cyberdrop Downloader v$VER by @hugogomess$RESET";
+echo "";
 
 apt-get update;
 apt-get install -y curl wget;
