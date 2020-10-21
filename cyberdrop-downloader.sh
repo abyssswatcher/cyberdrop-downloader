28e95d700cd51ab60fce1bc1199869e8f9085d2c
Hugo <hugogomes02@gmail.com>
2020-10-21 01:15:22 -0300

    remove figlet and uppercase vars

--- cyberdrop-downloader.sh
+++ cyberdrop-downloader.sh
@@ -17,34 +17,32 @@ COLOR1='\e[94m';
 RESET='\e[0m';
 
 if [ "$1" == "" ] || [ "$1" == "-h" ]; then
-    echo '';
-    figlet -t -c cyberdrop downloader
-    echo '';
+    #echo '          _                _                     _                 _              _          ';
+    #echo '  __ _  _| |__  ___ _ _ __| |_ _ ___ _ __ ___ __| |_____ __ ___ _ | |___  __ _ __| |___ _ _  ';
+    #echo ' / _| || | '_ \/ -_) '_/ _` | '_/ _ \ '_ \___/ _` / _ \ V  V / ' \| / _ \/ _` / _` / -_) '_| ';
+    #echo ' \__|\_, |_.__/\___|_| \__,_|_| \___/ .__/   \__,_\___/\_/\_/|_||_|_\___/\__,_\__,_\___|_|   ';
+    #echo '     |__/                           |_|                                                      ';
+    #echo '';
     echo -e "$COLOR1 + -- --=[ Cyberdrop Downloader v$VER by @hugogomess$RESET";
     echo -e "$COLOR1 + -- --=[ Usage: ./cyberdrop-downloader.sh <cyberdrop-link>$RESET";
     echo -e "$COLOR1 + -- --=[ Multiple Files: ./cyberdrop-downloader.sh -m$RESET";
 elif [ "$1" == "-m" ]; then
     echo '';
-    figlet -t -c cyberdrop downloader
-    echo '';
     echo -e "$COLOR1 + -- --=[ Cyberdrop Downloader v$VER by @hugogomess$RESET";
     echo -e "$COLOR1 + -- --=[ Usage: ./cyberdrop-downloader.sh <cyberdrop-link>$RESET";
     echo '';
 
-    filename=$2
-    while read line; do
-        ALBUM_NAME=$(curl "$line" | grep 'title has-text-centered' | cut -d '"' -f6 | head -n1);
+    FILENAME=$2
+    while read LINE; do
+        ALBUM_NAME=$(curl "$LINE" | grep 'title has-text-centered' | cut -d '"' -f6 | head -n1);
         mkdir "$ALBUM_NAME" && cd "$ALBUM_NAME";
 
-        curl "$line" | grep 'id="file"' | cut -d '"' -f6 > LINKS;
+        curl "$LINE" | grep 'id="file"' | cut -d '"' -f6 > LINKS;
         wget -i LINKS -q --show-progress;
         rm LINKS;
         cd "..";
-    done < $filename;
+    done < $FILENAME;
 else
-    echo '';
-    figlet -t -c cyberdrop downloader
-    echo '';
     echo -e "$COLOR1 + -- --=[ Cyberdrop Downloader v$VER by @hugogomess$RESET";
     echo -e "$COLOR1 + -- --=[ Usage: ./cyberdrop-downloader.sh <cyberdrop-link>$RESET";
     echo '';
